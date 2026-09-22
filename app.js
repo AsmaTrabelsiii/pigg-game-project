103f74b0f96a20352071769405d4e3070f4068b4
AsmaTrabelsiii <asmatrabelsi1717@gmail.com>
2026-09-22 20:58:39 +0100

    done

--- app.js
+++ app.js
@@ -1,63 +1,90 @@
-let dice=Math.trunc(Math.random()*6+1)
-let currenScore=document.querySelector("current-score-0")
-currenScore=0
+"use strict"
+const rollBtn = document.querySelector(".btn-roll");
 
-// roll-dice
-document.querySelector(".roll-dice").addEventListener("click",function(){
-    
-    if (dice==1) 
-       { document.querySelector("img").src="dice-1.png"
-           currenScore=0
-    document.querySelector(".current-score-0").textContent=currenScore
-       }
-    else if(dice==2)
-      {  document.querySelector("img").src="dice-2.png"
-        currenScore+=2
-    document.querySelector(".current-score-0").textContent=currenScore
-    dice=Math.trunc(Math.random()*6+1)}
-    else if (dice==3)
-   { document.querySelector("img").src="dice-3.png"
-    currenScore+=3
-    document.querySelector(".current-score-0").textContent=currenScore
-dice=Math.trunc(Math.random()*6+1)}
- 
-     else if (dice==4)
-  {  document.querySelector("img").src="dice-4.png"
-     currenScore+=4
-     document.querySelector(".current-score-0").textContent=currenScore
-dice=Math.trunc(Math.random()*6+1)}
+let player0 = document.querySelector(".player-0");
+let player1 = document.querySelector(".player-1");
 
-     else if (dice==5)
-    {document.querySelector("img").src="dice-5.png"
-         currenScore+=5
-         document.querySelector(".current-score-0").textContent=currenScore
-dice=Math.trunc(Math.random()*6+1)}
-     else 
-    document.querySelector("img").src="dice-6.png"
- currenScore+=6
- document.querySelector(".current-score-0").textContent=currenScore
-dice=Math.trunc(Math.random()*6+1)
+let score0 = document.querySelector(".score-0");
+let score1 = document.querySelector(".score-1");
+
+let diceImg = document.querySelector("img");
+
+let current0 = document.querySelector(".current-score-0");
+let current1 = document.querySelector(".current-score-1");
 
 
-})
+
+score0.textContent = 0;
+score1.textContent = 0;
+
+diceImg.classList.add("hidden");
+let  currentScore = 0;
+
+
+let activePlayer = 0;
+
+///// roll 
+
+rollBtn.addEventListener("click", function () {
+  const diceNumber = Math.trunc(Math.random() * 6) + 1;
+
+
+  diceImg.src = `dice-${diceNumber}.png`;
+  diceImg.classList.remove("hidden");
+  if (diceNumber !== 1) {
+  
+    currentScore += diceNumber;
+    document.querySelector(`.current-score-${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    currentScore = 0;
+    document.querySelector(`.current-score-${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player1.classList.toggle("active");
+    player0.classList.toggle("active");
+  }
+});
+
+
+
+
 // hold
-let score=document.querySelector("score-1").textContent
-document.querySelector(".hold").addEventListener("click",function(){
-score=+currenScore
-document.querySelector("score-1").textContent=score
-if (score>=100) {
+document.querySelector(".btn-hold").addEventListener("click", function () {
 
+  let scoreTotal = Number(
+    document.querySelector(`.score-${activePlayer}`).textContent
+  );
+
+  scoreTotal += currentScore;
+
+  document.querySelector(`.score-${activePlayer}`).textContent = scoreTotal;
+
+  if (scoreTotal >= 100) {
+    document.querySelector("main").style.backgroundColor = "green";
+    prompt("you winn!! 🥇 do you want to play another round!!")
+   
     
-}
-})
+  } else {
 
+    currentScore = 0;
 
+    document.querySelector(
+      `.current-score-${activePlayer}`
+    ).textContent = 0;
 
+    activePlayer = activePlayer === 0 ? 1 : 0;
 
-
-
-
+    player1.classList.toggle("active");
+    player0.classList.toggle("active");
+  }
+});
 //new game
-document.querySelector(".new game").addEventListener("click",function toggleSidebar() {
-        active.classList.toggle("active");
-      })
+
+
+document.querySelector(".btn-new-game").addEventListener("click", function () {
+  document.querySelector("main").style.backgroundColor = "rgb(255, 255, 255, 0.3)"
+  score0.textContent = 0;
+score1.textContent = 0;
+ current1.textContent=0
+  current0.textContent=0
+})
